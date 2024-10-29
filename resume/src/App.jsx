9dbc4af75aa04135e5cd13a6f11857bb5642cb05
Martin Bradbury <martinbradbury@btinternet.com>
2024-10-29 11:22:06 +0000

    initial commit - created app with vite and installed react bootstrap and tested.

---
+++ resume/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Button from 'react-bootstrap/Button';
+
+function App() {
+
+  return (
+    <>
+    <Button variant="primary">Primary</Button>{' '}
+      
+    </>
+  )
+}
+
+export default App
