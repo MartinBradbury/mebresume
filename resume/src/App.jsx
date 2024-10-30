8242e35a2af37dca142c42cf0d5d6948fe22f5d3
Martin Bradbury <martinbradbury@btinternet.com>
2024-10-30 15:16:01 +0000

    added basic nav bar and moved the heroimage into its own component and updated the imports.

--- resume/src/App.jsx
+++ resume/src/App.jsx
@@ -5,49 +5,17 @@ import styles from './styles/App.module.css';
 import Card from 'react-bootstrap/Card';
 import { useEffect, useState } from 'react';
 import profilepic from './assets/profilepic.jpg'
+import NavBar from './components/NavBar';
+import HeroImage from './components/HeroImage';
 
 function App() {
-  const [visible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setIsVisible(true), 1000); // Short delay to trigger transition
-    return () => clearTimeout(timer);
-  }, []);
+  
 
   return (
-    <Container>
-      <Row>
-        <Col className={styles.Col}>
-          <Card className={`${styles.CardOne} ${visible ? styles.visible : ''}`}>
-          <Card.Img variant="top" src={profilepic} />
-          </Card>
-        </Col>
-        <Col className={styles.Col}>
-          <Card className={`${styles.CardTwo} ${visible ? styles.visible : ''}`}>
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row>
-      <Col className={styles.Col}>
-      <Card className={`${styles.CardThree} ${visible ? styles.visible : ''}`}>
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-      </Col>
-      </Row>
-    </Container>
+    <>
+    <NavBar />
+    <HeroImage />
+    </>
   );
 }
 
---
+++ resume/src/components/HeroImage.jsx
@@ -0,0 +1,53 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import styles from '../styles/App.module.css';
+import Card from 'react-bootstrap/Card';
+import { useEffect, useState } from 'react';
+import profilepic from '../assets/profilepic.jpg'
+
+function HeroImage() {
+    const [visible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsVisible(true), 1000); // Short delay to trigger transition
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <Container>
+      <Row>
+        <Col className={styles.Col}>
+          <Card className={`${styles.CardOne} ${visible ? styles.visible : ''}`}>
+          <img className={styles.img} variant="top" src={profilepic} />
+          </Card>
+        </Col>
+        <Col className={styles.Col}>
+          <Card className={`${styles.CardTwo} ${visible ? styles.visible : ''}`}>
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+      <Col className={styles.Col}>
+      <Card className={`${styles.CardThree} ${visible ? styles.visible : ''}`}>
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+      </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default HeroImage
