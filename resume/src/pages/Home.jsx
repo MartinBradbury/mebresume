02675b3bd885010b7d2f76727c8201271225aea3
Martin Bradbury <martinbradbury@btinternet.com>
2024-11-05 15:46:27 +0000

    added repeat card in projects to aid with styling

--- resume/src/pages/Home.jsx
+++ resume/src/pages/Home.jsx
@@ -141,6 +141,83 @@ function Home() {
             </Card>
           </Col>
         </Row>
+        <Row className="justify-content-center text-center">
+          <Col xs={12} sm={10} md={8} lg={6}>
+            <Card
+              className={styles.card}
+              onMouseEnter={handleMouseEnterVideo1}
+              onMouseLeave={handleMouseLeaveVideo1}
+              onClick={handleShow} // Click to show Offcanvas
+            >
+              <video
+                ref={videoRef1} // Attach the ref to the video element
+                className={styles.video}
+                muted
+                loop
+                playsInline
+                webkit-playsinline="true"
+                autoPlay={isMobile} // Autoplay on mobile only
+                alt="triathlon background video"
+              >
+                <source src={video} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </Card>
+            {/* Offcanvas Component */}
+            <Offcanvas
+              className={styles.canvas}
+              show={show}
+              onHide={handleClose}
+            >
+              <Offcanvas.Header className={styles.whiteCloseButton} closeButton>
+                <Offcanvas.Title className={styles.canvasTitle}>
+                  <p>Tri Hub</p>
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <p>
+                  TriHub is a comprehensive platform created by Martin Bradbury,
+                  designed to serve the diverse needs of the triathlon
+                  community. It aims to bring together triathletes of all skill
+                  levels by offering a range of features, including performance
+                  tracking, customizable training plans, and a vibrant social
+                  space for sharing experiences. The platform emphasizes user
+                  engagement, providing tools for setting goals, interacting
+                  with the community, and staying updated with the latest
+                  industry news. TriHub's design focuses on accessibility and
+                  usability for competitive athletes, casual participants, and
+                  newcomers alike. The platform features a responsive layout and
+                  a clean, straightforward color scheme, ensuring a seamless
+                  experience across all devices. Continuous improvement is a
+                  priority, with future updates planned to enhance user
+                  satisfaction and expand platform capabilities.
+                  <br />
+                  <br />
+                  <h1>Technologies Used:</h1>
+                  <strong>
+                    Frontend: React, Axios, React Bootstrap Backend: Python,
+                    Django REST Additional Features: Responsive design
+                  </strong>
+                </p>
+              </Offcanvas.Body>
+            </Offcanvas>
+
+            {/* Additional Cards */}
+          </Col>
+          <Col>
+            <Card className={styles.cardBodyText}>
+              <Card.Body>
+                <Card.Title className={styles.cardBodyTextPosition}>
+                  Tri Hub
+                </Card.Title>
+                <Card.Text>
+                  A Full Stack project using React.js front end and Django REST
+                  back end.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
